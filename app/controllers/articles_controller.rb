f9eef417797826e0b3cab572fdf4c54332c02c38
Tim Flood <tflood9@gmail.com>
2021-01-15 17:14:34 -0500

    inital setup of articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+require 'wikipedia'
+require 'pry'
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    article = Wikipedia.find(params[:article][:title])
+    @article = Article.new(title: article.title, summary: article.summary, text: article.text, url: article.fullurl)
+    @article.save
+    redirect_to articles_path
+  end
+end
